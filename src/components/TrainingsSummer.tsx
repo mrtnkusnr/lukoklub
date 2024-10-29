e9837cbf074a567d96cbf8b8b6bf978a682d0626
Martin Kušnír <martin.kusnir999@gmail.com>
2024-10-29 14:54:59 +0100

    chore: design for summer trainings card

--- src/components/TrainingsSummer.tsx
+++ src/components/TrainingsSummer.tsx
@@ -1,48 +1,82 @@
 const TrainingsSummer = () => {
-  const summerLocation = "Rampová 7, Košice";
+  const summerLocation =
+    "Bývalý areál Vojenskej leteckej akadémie Milana Rastislava Štefánika - vchod cez Okresné riaditeľstvo PZ v Košiciach, Rampová 7, Košice";
 
-  const summerDateRange = "9. máj - 1. júl";
-  const summerSchedule = "16:30 - 18:00";
+  const summerDateRange1 = "09. máj - 01. júl";
+  const summerDateRange2 = "01. júl - 27. okt";
+  const summerSchedule1 = "16:30 - 18:00";
+  const summerSchedule2 = "17:00 - 18:30";
   const summerDays = "Pondelok, Streda, Piatok*";
 
   return (
-    <div className="p-6 mt-24">
-      <div className="flex flex-col text-center items-center border border-gray-200 w-[36rem] shadow-sm">
-        <h1 className="text-5xl font-semibold my-8">Letná sezóna</h1>
-        <div className="flex sm:flex-row gap-6 sm:gap-3 my-2 w-48 items-center">
-          <span className="material-symbols-outlined text-4xl">date_range</span>
-          <div className="flex flex-col items-start">
-            <span className="font-semibold text-lg">Dátumy:</span>
-            <span className="sm:text-left"> {summerDateRange}</span>
+    <div className="mt-24">
+      <div className="flex flex-col text-center items-center p-8 border border-gray-200 shadow-sm ">
+        <h1 className="text-5xl font-semibold mt-8 mb-14">Letná sezóna</h1>
+        <div className="flex flex-row w-full justify-between pb-12 border-b-2">
+          <div className="flex sm:flex-row gap-3 items-center">
+            <span className="material-symbols-outlined text-5xl">
+              date_range
+            </span>
+            <div className="flex flex-col items-start">
+              <span className="font-semibold text-lg">Dátumy:</span>
+              <span className="sm:text-left"> {summerDateRange1}</span>
+            </div>
+          </div>
+          <div className="flex sm:flex-row gap-3 items-center">
+            <span className="material-symbols-outlined text-5xl">today</span>
+            <div className="flex flex-col items-start">
+              <span className="font-semibold text-lg">Dni v týždni:</span>
+              <span className="sm:text-left"> {summerDays}</span>
+            </div>
+          </div>
+          <div className="flex sm:flex-row gap-3 items-center">
+            <span className="material-symbols-outlined text-5xl">schedule</span>
+            <div className="flex flex-col items-start">
+              <span className="font-semibold text-lg">Čas tréningu:</span>
+              <span className="sm:text-left"> {summerSchedule1}</span>
+            </div>
           </div>
         </div>
-        <div className="flex sm:flex-row gap-6 sm:gap-3 my-2 w-48 items-center">
-          <span className="material-symbols-outlined text-4xl">schedule</span>
-          <div className="flex flex-col items-start">
-            <span className="font-semibold text-lg">Čas:</span>
-            <span className="sm:text-left"> {summerSchedule}</span>
+
+        <div className="flex flex-row w-full justify-between mt-12 pb-12 border-b-2">
+          <div className="flex sm:flex-row gap-3 items-center">
+            <span className="material-symbols-outlined text-5xl">
+              date_range
+            </span>
+            <div className="flex flex-col items-start">
+              <span className="font-semibold text-lg">Dátumy:</span>
+              <span className="sm:text-left"> {summerDateRange2}</span>
+            </div>
+          </div>
+          <div className="flex sm:flex-row gap-3 items-center">
+            <span className="material-symbols-outlined text-5xl">event</span>
+            <div className="flex flex-col items-start">
+              <span className="font-semibold text-lg">Dni v týždni:</span>
+              <span className="sm:text-left"> {summerDays}</span>
+            </div>
+          </div>
+          <div className="flex sm:flex-row gap-3 items-center">
+            <span className="material-symbols-outlined text-5xl">schedule</span>
+            <div className="flex flex-col items-start">
+              <span className="font-semibold text-lg">Čas tréningu:</span>
+              <span className="sm:text-left"> {summerSchedule2}</span>
+            </div>
           </div>
         </div>
-        <div className="flex sm:flex-row gap-6 sm:gap-3 my-2 w-48 items-center">
-          <span className="material-symbols-outlined text-4xl">target</span>
-          <div className="flex flex-col items-start">
-            <span className="font-semibold text-lg">Dni:</span>
-            <span className="sm:text-left"> {summerDays}</span>
-          </div>
-        </div>
-        <div className="flex sm:flex-row gap-6 sm:gap-3 my-2 w-48 items-center">
-          <span className="material-symbols-outlined text-4xl">
+
+        <div className="flex sm:flex-row gap-3 mt-12 items-center">
+          <span className="material-symbols-outlined text-5xl">
             location_on
           </span>
           <div className="flex flex-col items-start">
             <span className="font-semibold text-lg">Miesto:</span>
-            <span className="sm:text-left"> {summerLocation}</span>
+            <span className="sm:text-left max-w-xl"> {summerLocation}</span>
           </div>
         </div>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d598.9192070298299!2d21.269447228329586!3d48.73537760238559!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473ee0857baf87ad%3A0xb4591f084d7c5b7f!2sPol%C3%ADcia%20SR%20-%20Okresn%C3%A9%20riadite%C4%BEstvo%20PZ%20v%20Ko%C5%A1iciach!5e0!3m2!1sen!2ssk!4v1729106767531!5m2!1sen!2ssk"
           loading="lazy"
-          className="border border-gray-200 rounded-md shadow-sm w-96 mt-6"
+          className="border border-gray-200 rounded-md shadow-sm w-full mt-12 mb-8 grayscale hover:grayscale-0"
         ></iframe>
       </div>
     </div>
